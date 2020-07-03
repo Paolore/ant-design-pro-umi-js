2d9ada14bd2ed02a706c83a7b387e4a828ccbf64
Lorena Correa <paolorena3@gmail.com>
2020-07-02 19:40:59 -0500

    Update index.tsx

--- src/pages/form/step-form/index.tsx
+++ src/pages/form/step-form/index.tsx
@@ -44,7 +44,7 @@ class StepForm extends Component<StepFormProps> {
       stepComponent = <Step1 />;
     }
     return (
-      <PageHeaderWrapper content="将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成。">
+      <PageHeaderWrapper content="">
         <Card bordered={false}>
           <>
             <Steps current={currentStep} className={styles.steps}>
